247a74acdba46c53fdec5591a8268717abf12224
Corso <58090876+yosbel-penate@users.noreply.github.com>
2023-11-03 10:32:13 -0400

    +++

--- flaskr/poll/model/query_delete_question_by_author_id.py
+++ flaskr/poll/model/query_delete_question_by_author_id.py
@@ -2,5 +2,5 @@ from ..views.blueprint import g, get_db
 
 
 def query_delete_question_by_author_id(db):
-    db.execute('DELETE FROM question WHERE author_id = ?', (g.user['id'],))
+    db.execute('DELETE FROM question WHERE author_id = ? AND is_checked = 1 ', (g.user['id'],))
     db.commit()
